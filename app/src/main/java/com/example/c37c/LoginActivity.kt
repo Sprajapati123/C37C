ce8464f830b0ece0c925f48499ba35d18530f8c6
Sprajapati123 <sandisprajapati52@gmail.com>
2025-11-16 09:20:57 +0545

    login

---
+++ app/src/main/java/com/example/c37c/LoginActivity.kt
@@ -0,0 +1,259 @@
+package com.example.c37c
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.c37c.ui.theme.Black
+import com.example.c37c.ui.theme.Blue
+import com.example.c37c.ui.theme.C37CTheme
+import com.example.c37c.ui.theme.PurpleGrey80
+import com.example.c37c.ui.theme.White
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginBody()
+        }
+    }
+}
+
+@Composable
+fun LoginBody() {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var visibility by remember { mutableStateOf(false) }
+
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .background(White)
+        ) {
+            Spacer(modifier = Modifier.height(50.dp))
+            Text(
+                "Sign In",
+                style = TextStyle(
+                    textAlign = TextAlign.Center,
+                    fontSize = 24.sp,
+                    color = Blue,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Text(
+                "This is lorem ipsum, this is ecommerce here you can buy any produycts you want",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp),
+                style = TextStyle(
+                    textAlign = TextAlign.Center,
+                    color = Black.copy(0.5f)
+                )
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp)
+            ) {
+                SocialMediaCard(
+                    Modifier
+                        .height(60.dp)
+                        .weight(1f),
+                    R.drawable.face,
+                    "Facebook"
+                )
+                Spacer(modifier = Modifier.width(20.dp))
+
+                SocialMediaCard(
+                    Modifier
+                        .height(60.dp)
+                        .weight(1f),
+                    R.drawable.gmail,
+                    "Gmail"
+                )
+            }
+
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 40.dp, horizontal = 20.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                HorizontalDivider(
+                    modifier = Modifier.weight(1f)
+                )
+
+                Text("OR", modifier = Modifier.padding(horizontal = 20.dp))
+                HorizontalDivider(
+                    modifier = Modifier.weight(1f)
+                )
+            }
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { data ->
+                    email = data
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email
+                ),
+                placeholder = {
+                    Text("abc@gmail.com")
+                },
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = PurpleGrey80,
+                    focusedContainerColor = PurpleGrey80,
+                    focusedIndicatorColor = Blue,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp),
+                shape = RoundedCornerShape(15.dp)
+            )
+
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { data ->
+                    password = data
+                },
+                placeholder = {
+                    Text("********")
+                },
+                trailingIcon = {
+                    IconButton(onClick = {
+                        visibility = !visibility
+                    }) {
+                        Icon(
+                            painter = if (visibility)
+                                painterResource(R.drawable.baseline_visibility_off_24)
+                            else
+                                painterResource(R.drawable.baseline_visibility_24),
+                            contentDescription = null
+                        )
+                    }
+                },
+                visualTransformation = if (!visibility) PasswordVisualTransformation() else VisualTransformation.None,
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = PurpleGrey80,
+                    focusedContainerColor = PurpleGrey80,
+                    focusedIndicatorColor = Blue,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp),
+                shape = RoundedCornerShape(15.dp)
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text("Forget Password", modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 15.dp), style = TextStyle(textAlign = TextAlign.End))
+
+            Button(onClick = {},
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 15.dp, vertical = 20.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Blue
+                )
+                ) {
+                Text("Log In")
+            }
+
+            Text(buildAnnotatedString {
+                append("Don't have account? ")
+
+                withStyle(SpanStyle(color = Blue)){
+                    append("Sign up")
+                }
+            })
+        }
+    }
+}
+
+@Composable
+fun SocialMediaCard(modifier: Modifier, image: Int, label: String) {
+    Card(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(image),
+                contentDescription = null,
+                modifier = Modifier.size(30.dp)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(label)
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun LoginPreview() {
+    LoginBody()
+}
--- app/src/main/java/com/example/c37c/ui/theme/Color.kt
+++ app/src/main/java/com/example/c37c/ui/theme/Color.kt
@@ -4,7 +4,10 @@ import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
 val CreamWhite = Color(0xFFD6D5D9)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Blue = Color(0xFF5A75CD)
+val PurpleGrey80 = Color(0xFFE2DBED)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
